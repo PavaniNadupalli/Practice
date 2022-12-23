607492ded3e21c1f25a7326cd79a8112a09313b7
PavaniNadupalli <117763882+PavaniNadupalli@users.noreply.github.com>
2022-12-23 18:38:51 +0530

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,20 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+        String hungry = "yes";
+		
+		if(hungry == "yes")
+		{
+			System.out.println("have a nice food");
+		}
+		else
+		{
+			System.out.println("enjoy your day");
+		}
+
+
+	}
+
+}
